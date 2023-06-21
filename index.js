f9841ad2080770c367931553ce158e76479da4a8
USER <+>
2023-06-21 10:47:10 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  function superBowl() {
+    const records = [
+      { year: "2017", result: "W" },
+      { year: "2018", result: "undefined" },
+      { year: "2019", result: "L" }
+    ];
+    return superbowlWin(records);
+  }
+  
+  console.log(superBowl());
